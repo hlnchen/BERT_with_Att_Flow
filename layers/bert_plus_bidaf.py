ef832e342760241d265fb47c619d2ae94074c337
Haolin Chen <hlnchen@ucdavis.edu>
2020-12-10 14:45:21 -0800

    remove CNN parts; add support on attention map; fixed LSTM

--- layers/bert_plus_bidaf.py
+++ layers/bert_plus_bidaf.py
@@ -25,7 +25,7 @@ except ModuleNotFoundError:
 # from transformers import BertPreTrainedModel, BertModel, BertTokenizer
 
 class BERT_plus_BiDAF(nn.Module):
-    def __init__(self, question_len = 62, if_cnn = False, if_extra_modeling = False):
+    def __init__(self, question_len = 62, if_extra_modeling = False, if_bidrectional = False, if_attention_map = False):
         """
         question_len: the length of the question in the padded sequence
         if_cnn: if to use a character-level cnn encoder. Default: false
@@ -35,38 +35,28 @@ class BERT_plus_BiDAF(nn.Module):
         super().__init__()
         # Constants
         self.hidden_dim = 768   # dimension d: because of BERT
-        self.vocab_size = None  # TODO: use BertTokenizer.vocab_size
         self.question_len = question_len
+        self.if_attention_map = if_attention_map
+
         # Network modules
         # BERT
         self.bert_layer = BertModel.from_pretrained('bert-base-uncased')
-        # CNN
-        """ TODO: add CNN embedding layer"""
-        if if_cnn:
-            self.cnn = CharCNN(input_length=512, vocab_size=self.vocab_size) # because of BERT
-        else:
-            self.cnn = None
 
         # Bidirectional attention
-        if self.cnn:
-            self.attention_layer = AttFlow(feature_dimension=2*self.hidden_dim)
-        else:
-            self.attention_layer = AttFlow(feature_dimension=self.hidden_dim)
+        self.attention_layer = AttFlow(feature_dimension=self.hidden_dim, if_attention_map = self.if_attention_map)
         
         # Additional modeling layer LSTM/Transformer:
         if if_extra_modeling:
-            if self.cnn:
-                self.modeling_layer = nn.LSTM(input_size=8*self.hidden_dim,hidden_size=2*self.hidden_dim, num_layers=2)
-            else:
-                self.modeling_layer = nn.LSTM(input_size=4*self.hidden_dim,hidden_size=2*self.hidden_dim, num_layers=2)
+            self.modeling_layer = nn.LSTM(input_size=4*self.hidden_dim,hidden_size=2*self.hidden_dim, num_layers=2, batch_first = True, bidirectional= if_bidrectional)
         else:
             self.modeling_layer = None
             
         # Prediction
         if self.modeling_layer:
-            self.prediction_layer = PredictionLayer(feature_dimension=2*self.hidden_dim)
-        elif self.cnn:
-            self.prediction_layer = PredictionLayer(feature_dimension=8*self.hidden_dim)
+            if self.modeling_layer.bidirectional == True:
+                self.prediction_layer = PredictionLayer(feature_dimension=4*self.hidden_dim)
+            else:
+                self.prediction_layer = PredictionLayer(feature_dimension=2*self.hidden_dim)
         else:
             self.prediction_layer = PredictionLayer(feature_dimension=4*self.hidden_dim)
 
@@ -85,36 +75,21 @@ class BERT_plus_BiDAF(nn.Module):
         bert_features, _ = self.bert_layer(input_ids = input_ids, token_type_ids = None, attention_mask = input_mask, output_all_encoded_layers=False) # (N,L,d)
         # Separate features
         bert_question_features = bert_features[:, 1:self.question_len+1,:] # (N,J,d) J=62
-        bert_context_features = torch.cat((bert_features[:, 0, :].unsqueeze(dim=1), bert_features[:, self.question_len+1:, :]), dim=1) # (N,T,d), T=450 NOTE: T+J = 512， context is [CLS][SEP][context][SEP]
-        
-        
-        # Feed into CNN
-        if self.cnn:
-            """ TODO: check the validity here """
-            cnn_features = self.cnn(input_ids)
-            """ TODO: separate CNN features """
-            cnn_context_features = None
-            cnn_question_features = None
+        bert_context_features = torch.cat((bert_features[:, 0, :].unsqueeze(dim=1), bert_features[:, self.question_len+1:, :]), dim=1) # (N,T,d), T=450 NOTE: T+J = 512， context is [CLS][context][SEP]
 
         # Concatenate and feed into attention
-        if self.cnn:
-            context_features = torch.cat((bert_context_features, cnn_context_features), dim = -1) # (N,T,2d)
-            question_features = torch.cat((bert_question_features, cnn_question_features), dim = -1) # (N,T,2d)
-            c2q_attention, q2c_attention = self.attention_layer(context_features, question_features) # (N,T,2d), (N,T,2d)
+        if self.if_attention_map:
+            c2q_attention, q2c_attention, weight_c2q, weight_q2c =  self.attention_layer(bert_context_features, bert_question_features)
         else:
             c2q_attention, q2c_attention = self.attention_layer(bert_context_features, bert_question_features) # (N,T,d), (N,J,d)
         
         # Combine all features and make prediction
-        if self.cnn:
-            combined_features = torch.cat((context_features, c2q_attention, 
-            torch.mul(context_features, c2q_attention), torch.mul(context_features, q2c_attention)), dim=-1) # (N,T,8d)
-        else:
-            combined_features = torch.cat((bert_context_features, c2q_attention, 
-            torch.mul(bert_context_features, c2q_attention), torch.mul(bert_context_features, q2c_attention)), dim = -1) # (N,T,4d)
+        combined_features = torch.cat((bert_context_features, c2q_attention, 
+        torch.mul(bert_context_features, c2q_attention), torch.mul(bert_context_features, q2c_attention)), dim = -1) # (N,T,4d)
 
         # If we use extra modeling layer
         if self.modeling_layer:
-            combined_features = self.modeling_layer(combined_features)[0] #(N,T,2d)
+            combined_features = self.modeling_layer(combined_features)[0] #(N,T,2d), or (N,T,4d) if bidirectional
         
         start_logits, end_logits = self.prediction_layer(combined_features) # (N,T), (N,T)
         if len(start_logits.shape) == 1:
@@ -132,7 +107,11 @@ class BERT_plus_BiDAF(nn.Module):
             end_loss = loss(end_logits, end_pos)
             total_loss = (start_loss + end_loss)/2
         
-        return total_loss, start_logits, end_logits
+        if self.if_attention_map:
+            return total_loss, start_logits, end_logits, weight_c2q, weight_q2c
+        else:
+            return total_loss, start_logits, end_logits
+        
 
 if __name__ == "__main__":
     model = BERT_plus_BiDAF(if_extra_modeling=True)
